d928754085ca98eb18a7e95d4ad26b0fd913cbde
lee <616798887@qq.com>
2019-12-24 17:01:02 +0800

    1，编写新成绩分析计算用例

--- testFrame/kcb_ana.py
+++ testFrame/kcb_ana.py
@@ -10,7 +10,13 @@ class Login(unittest.TestCase):
         self.b.p.openBrowser("http://www.521ke.com")
 
     def test001(self):
-        self.assertEquals(self.b.login(r.readXml("login","username1"),r.readXml("login","passwd1")),r.readXml("login","expect1"))
+        #登录成功校验
+        #self.assertEquals(self.b.login(r.readXml("login","username1"),r.readXml("login","passwd1")),r.readXml("login","expect1"))
+        try:
+            self.assertIsNotNone(self.b.login(r.readXml("login","username1"),r.readXml("login","passwd1")),"登录成功")
+        except NotImplementedError :
+            print("no")
+
 
     def tearDown(self):
         self.b.p.closeBrowser()
